290270d18f308b9e443c262f91b07287813a2d91
arsentyeva-n <110901516+arsentyeva-n@users.noreply.github.com>
2022-12-01 15:40:43 +0900

    Add files via upload

--- 4 лаба (циклы)/Project1.dpr
+++ 4 лаба (циклы)/Project1.dpr
@@ -6,36 +6,61 @@ uses
   SysUtils;
 
 var
-a: smallint;
-i,n,sum:smallint;
-mas: array of smallint;
+i,n,sum,a:integer;
+mas: array[1..100] of integer;
 
 begin
 readln(n);
-setlength(mas,n);
-for i:=0 to n-1 do
+//setlength(mas,n);
+for i:=1 to n do
   begin
     readln(a);
     mas[i]:=a;
   end;
-  n:=n-1;
-  
+
+  sum:=0;
+  {for i:=0 to n do
+  begin
+    write(mas[i],' ');
+  end;         }
+
   asm
+@_start:
+  mov ebx,0
+  //lea esi, [mas + 2 * ebx]
+  mov edx,0
+  mov eax,0
+  mov ecx,n
+
 @for_start:
-  lea bx, mas;	//берЄм в bx адрес первого элемента
-  mov cx, n;    //счетчик цикла
-  xor ax, ax;
-@begin:
-    add ax, [bx];
-    inc bx;    //переходим к следующему элементу
-    loop @begin;
-@for_finish:
-    mov sum,ax;
+    lea esi, [mas + ebx * 2]    // берем адрес
+    mov eax, [esi]            // добавл€ем элемент массива i в eax
+    push ecx                  //заносим ecx в стек, чтобы не потер€ть
+    mov ecx,5                 // в ecx заносим 5
+    div ecx                   //делим eax на  5
+    pop ecx                    //извлекаем из стека ecx старый
+    mov eax, [esi]             //eax снова равно значению элемента массива
+    cmp edx, 0                 //сраниваем остаток
+    jne @end_if                //если число делитьс€ на 5 то провер€ем не делитс€ ли на 7
+      push ecx
+      mov ecx,7
+      div ecx
+      pop ecx
+      mov eax, [esi]
+      cmp edx, 0
+      je @end_else
+        add sum,eax         //sum += mas[i]
+
+    @end_if:
+    @end_else:
+
+    inc ebx    //переходим к следующему элементу
+    loop @for_start
+
   end;
 
   writeln('sum = ', sum);
 
-  readln;
   readln;
 end.
 
